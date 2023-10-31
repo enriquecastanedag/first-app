5d874d569fdab1b972804b2501c469d4157b304f
Enrique Castañeda <enrique.castaneda@accenture.com>
2023-10-31 14:57:01 -0600

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HousingLocationComponent,
+    DetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
